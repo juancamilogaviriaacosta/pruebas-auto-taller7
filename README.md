d7579d310e27110847bc23b22803eecbbc21e717
juan <juancamilogaviriaacosta@gmail.com>
2017-10-26 23:48:28 -0500

    adb java

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pruebas-auto-taller7
+
+### Ejecutar monkey java
+
+java -jar monkey_adb_juan_gaviria.jar n=10 tap=0.3 text=0.2 swipe=0.4 keyevent=0.1 rotate=0.3 network_speed=0.2 accelerometer=0.1 apk=/home/juan/Escritorio/ejemplo.apk paquete=co.com.algo
--- monkey_adb_juan_gaviria/src/monkey/adb/Main.java
+++ monkey_adb_juan_gaviria/src/monkey/adb/Main.java
@@ -3,55 +3,106 @@ package monkey.adb;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.UUID;
 
 /**
- *
  * @author juan
  */
 public class Main {
-
 
     public static final String ADB_ROOT = "/home/juan/Android/Sdk/platform-tools/";
     public static final String ADB_INPUT = ADB_ROOT + "adb shell input ";
     public static final String TELNET_TOKEN = "ZhOfLHqRnR2sPche";
     public static final String EMULATOR_PORT = "5554";
+    public static final String[] VELOCIDADES = {"gsm", "hscsd", "gprs", "edge", "umts", "hsdpa", "lte", "evdo", "full"};
 
-    private static BufferedWriter connectToTelnet() throws IOException {
+    public static void ejecutarTelnet(String orden) throws IOException {
         Runtime rt = Runtime.getRuntime();
-        Process telnet = rt.exec("telnet localhost "+EMULATOR_PORT);
-        return new BufferedWriter(new OutputStreamWriter(telnet.getOutputStream()));
-    }
-
-    public static void rotate() throws IOException {
-        BufferedWriter out = connectToTelnet();
-        out.write("auth "+TELNET_TOKEN+"\n");
-        out.write("rotate\n");
+        Process telnet = rt.exec("telnet localhost " + EMULATOR_PORT);
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(telnet.getOutputStream()));
+        out.write("auth " + TELNET_TOKEN + "\n");
+        out.write(orden + "\n");
         out.write("quit\n");
         out.flush();
     }
 
-    public static void main(String[] args) {
+    public static void ejecutarAdb(String orden) throws IOException {
         Runtime rt = Runtime.getRuntime();
+        rt.exec(ADB_INPUT + orden);
+    }
+
+    public static void instalarApk(String apk, String paquete) throws IOException {
+        Runtime rt = Runtime.getRuntime();
+        rt.exec(ADB_ROOT + "adb shell install " + apk);
+        rt.exec(ADB_ROOT + "adb shell am start -n " + paquete);
+    }
+
+    public static boolean getEjecutar(String probabilidad) {
+        Random random = new Random();
+        return probabilidad != null && Double.valueOf(probabilidad) <= random.nextInt(100);
+    }
+
+    public static void main(String[] args) {
         try {
+            Map<String, String> argumentos = new HashMap<>();
+            for (String arg : args) {
+                argumentos.put(arg.substring(0, arg.indexOf('=')), arg.substring(arg.indexOf('=') + 1));
+            }
+            System.out.println(argumentos);
+            Integer n = Integer.valueOf(argumentos.get("n"));
+            String tap = argumentos.get("tap");
+            String text = argumentos.get("text");
+            String swipe = argumentos.get("swipe");
+            String keyevent = argumentos.get("keyevent");
+            String rotate = argumentos.get("rotate");
+            String network_speed = argumentos.get("network_speed");
+            String accelerometer = argumentos.get("accelerometer");
+            String apk = argumentos.get("apk");
+            String paquete = argumentos.get("paquete");
 
+            instalarApk(apk, paquete);
             Random random = new Random(12345);
+            for (int i = 0; i < n; i++) {
+                int x1 = random.nextInt(1080);
+                int y1 = random.nextInt(1920);
+                int x2 = random.nextInt(1080);
+                int y2 = random.nextInt(1920);
+                int key = random.nextInt(283);
+                int vel = random.nextInt(8);
+                int x = random.nextInt(99);
+                int y = random.nextInt(99);
+                int z = random.nextInt(99);
 
-            int i = 0;
-            while(i < 10) {
-                int x = random.nextInt(1080);
-                int y = random.nextInt(1920);
-                int rotate = random.nextInt(2);
-
-                if (rotate == 1) {
-                    Main.rotate();
+                //Comandos ADB
+                if (getEjecutar(tap)) {
+                    ejecutarAdb("tap " + x1 + " " + y1);
+                }
+                if (getEjecutar(text)) {
+                    ejecutarAdb("text " + UUID.randomUUID());
+                }
+                if (getEjecutar(swipe)) {
+                    ejecutarAdb("swipe " + x1 + " " + y1 + " " + x2 + " " + y2);
+                }
+                if (getEjecutar(keyevent)) {
+                    ejecutarAdb("keyevent " + key);
                 }
 
-                rt.exec(ADB_INPUT + "tap " + x + " " + y);
-                i++;
+                // Comandos telnet
+                if (getEjecutar(rotate)) {
+                    ejecutarTelnet("rotate");
+                }
+                if (getEjecutar(network_speed)) {
+                    ejecutarTelnet("network speed " + VELOCIDADES[vel]);
+                }
+                if (getEjecutar(accelerometer)) {
+                    ejecutarTelnet("sensor set acceleration " + x + ":" + y + ":" + z);
+                }
+
                 Thread.sleep(1000);
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }

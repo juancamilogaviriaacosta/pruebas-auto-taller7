7903b69aa9509208497d1e0dc14395410813e8a3
juan <juancamilogaviriaacosta@gmail.com>
2017-10-27 21:18:44 -0500

    FIN adb java

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ### Ejecutar monkey java
 
-java -jar monkey_adb_juan_gaviria.jar n=10 tap=0.3 text=0.2 swipe=0.4 keyevent=0.1 rotate=0.3 network_speed=0.2 accelerometer=0.1 apk=/home/juan/Escritorio/ejemplo.apk paquete=co.com.algo
+java -jar monkey_adb_juan_gaviria.jar adb_root=/home/juan/Android/Sdk/platform-tools/ telnet_token=ZhOfLHqRnR2sPche emulator_port=5554 n=10 tap=0.3 text=0.2 swipe=0.4 keyevent=0.1 rotate=0.3 network_speed=0.2 accelerometer=0.1 apk=/home/juan/Escritorio/silectric.apk paquete=net.alaindonesia.silectric           
--- monkey_adb_juan_gaviria/src/monkey/adb/Main.java
+++ monkey_adb_juan_gaviria/src/monkey/adb/Main.java
@@ -1,7 +1,11 @@
 package monkey.adb;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,36 +17,53 @@ import java.util.UUID;
  */
 public class Main {
 
-    public static final String ADB_ROOT = "/home/juan/Android/Sdk/platform-tools/";
-    public static final String ADB_INPUT = ADB_ROOT + "adb shell input ";
-    public static final String TELNET_TOKEN = "ZhOfLHqRnR2sPche";
-    public static final String EMULATOR_PORT = "5554";
-    public static final String[] VELOCIDADES = {"gsm", "hscsd", "gprs", "edge", "umts", "hsdpa", "lte", "evdo", "full"};
+    private static String adb_root;
+    private static String telnet_token;
+    private static String emulator_port;
+    private static final String[] VELOCIDADES = {"gsm", "hscsd", "gprs", "edge", "umts", "hsdpa", "lte", "evdo", "full"};
 
     public static void ejecutarTelnet(String orden) throws IOException {
         Runtime rt = Runtime.getRuntime();
-        Process telnet = rt.exec("telnet localhost " + EMULATOR_PORT);
+        Process telnet = rt.exec("telnet localhost " + emulator_port);
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(telnet.getOutputStream()));
-        out.write("auth " + TELNET_TOKEN + "\n");
+        out.write("auth " + telnet_token + "\n");
         out.write(orden + "\n");
         out.write("quit\n");
         out.flush();
+        System.out.println("Orden telnet: " + orden);
     }
 
     public static void ejecutarAdb(String orden) throws IOException {
         Runtime rt = Runtime.getRuntime();
-        rt.exec(ADB_INPUT + orden);
+        rt.exec(adb_root + "adb shell input " + orden);
+        System.out.println("Orden adb: " + orden);
     }
 
-    public static void instalarApk(String apk, String paquete) throws IOException {
+    public static void instalarApk(String apk, String paquete) throws IOException, InterruptedException {
         Runtime rt = Runtime.getRuntime();
-        rt.exec(ADB_ROOT + "adb shell install " + apk);
-        rt.exec(ADB_ROOT + "adb shell am start -n " + paquete);
+        rt.exec(adb_root + "adb install -r " + apk);
+        System.out.println("Instaldo: " + apk);
+        Thread.sleep(3000);
     }
 
-    public static boolean getEjecutar(String probabilidad) {
+    public static void abrirApk(String paquete) throws IOException {
+        Runtime rt = Runtime.getRuntime();
+        InputStream is = rt.exec(adb_root + "adb shell dumpsys window windows | grep -E 'mCurrentFocus|mFocusedApp'").getInputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(is));
+        String salida = "";
+        String line;
+        while ((line = in.readLine()) != null) {
+            salida += line;
+        }
+        if (!salida.contains(paquete)) {
+            rt.exec(adb_root + "adb shell monkey -p " + paquete + " 1");
+            System.out.println("Abriendo: " + paquete);
+        }
+    }
+
+    public static boolean getPermisoEjecutar(String probabilidad) {
         Random random = new Random();
-        return probabilidad != null && Double.valueOf(probabilidad) <= random.nextInt(100);
+        return probabilidad != null && random.nextInt(101) <= (Double.valueOf(probabilidad) * 100);
     }
 
     public static void main(String[] args) {
@@ -51,7 +72,9 @@ public class Main {
             for (String arg : args) {
                 argumentos.put(arg.substring(0, arg.indexOf('=')), arg.substring(arg.indexOf('=') + 1));
             }
-            System.out.println(argumentos);
+            adb_root = argumentos.get("adb_root");
+            telnet_token = argumentos.get("telnet_token");
+            emulator_port = argumentos.get("emulator_port");
             Integer n = Integer.valueOf(argumentos.get("n"));
             String tap = argumentos.get("tap");
             String text = argumentos.get("text");
@@ -64,6 +87,7 @@ public class Main {
             String paquete = argumentos.get("paquete");
 
             instalarApk(apk, paquete);
+            abrirApk(paquete);
             Random random = new Random(12345);
             for (int i = 0; i < n; i++) {
                 int x1 = random.nextInt(1080);
@@ -71,36 +95,42 @@ public class Main {
                 int x2 = random.nextInt(1080);
                 int y2 = random.nextInt(1920);
                 int key = random.nextInt(283);
-                int vel = random.nextInt(8);
+                int vel = random.nextInt(8 + 1);
                 int x = random.nextInt(99);
                 int y = random.nextInt(99);
                 int z = random.nextInt(99);
 
                 //Comandos ADB
-                if (getEjecutar(tap)) {
+                if (getPermisoEjecutar(tap)) {
+                    abrirApk(paquete);
                     ejecutarAdb("tap " + x1 + " " + y1);
                 }
-                if (getEjecutar(text)) {
+                if (getPermisoEjecutar(text)) {
+                    abrirApk(paquete);
                     ejecutarAdb("text " + UUID.randomUUID());
                 }
-                if (getEjecutar(swipe)) {
+                if (getPermisoEjecutar(swipe)) {
+                    abrirApk(paquete);
                     ejecutarAdb("swipe " + x1 + " " + y1 + " " + x2 + " " + y2);
                 }
-                if (getEjecutar(keyevent)) {
+                if (getPermisoEjecutar(keyevent)) {
+                    abrirApk(paquete);
                     ejecutarAdb("keyevent " + key);
                 }
 
                 // Comandos telnet
-                if (getEjecutar(rotate)) {
+                if (getPermisoEjecutar(rotate)) {
+                    abrirApk(paquete);
                     ejecutarTelnet("rotate");
                 }
-                if (getEjecutar(network_speed)) {
+                if (getPermisoEjecutar(network_speed)) {
+                    abrirApk(paquete);
                     ejecutarTelnet("network speed " + VELOCIDADES[vel]);
                 }
-                if (getEjecutar(accelerometer)) {
+                if (getPermisoEjecutar(accelerometer)) {
+                    abrirApk(paquete);
                     ejecutarTelnet("sensor set acceleration " + x + ":" + y + ":" + z);
                 }
-
                 Thread.sleep(1000);
             }
         } catch (Exception e) {

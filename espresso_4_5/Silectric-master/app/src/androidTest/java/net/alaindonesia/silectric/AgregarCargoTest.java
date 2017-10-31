b6ec1141b190f5eb091553b6a69bb1eb2fc1d48a
juan <juancamilogaviriaacosta@gmail.com>
2017-10-30 22:19:50 -0500

    espresso_4_5

--- espresso_4_5/Silectric-master/app/src/androidTest/java/net/alaindonesia/silectric/AgregarCargoTest.java
+++ espresso_4_5/Silectric-master/app/src/androidTest/java/net/alaindonesia/silectric/AgregarCargoTest.java
@@ -5,7 +5,13 @@ import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,11 +19,10 @@ import org.junit.runner.RunWith;
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -25,14 +30,14 @@ import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class CargoBasicoTest {
+public class AgregarCargoTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void cargoBasicoTest() {
-        pressBack();
+    public void agregarCargoTest() {
+        //pressBack();
 
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
 
@@ -41,17 +46,35 @@ public class CargoBasicoTest {
         appCompatTextView.perform(click());
 
         ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.basic_charge_fee_edit_text), withText("0.0"), isDisplayed()));
-        appCompatEditText.perform(replaceText("0.0250"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.basic_charge_fee_edit_text), withText("0.0250"), isDisplayed()));
-        appCompatEditText2.perform(pressImeActionButton());
+                allOf(withId(R.id.basic_charge_fee_edit_text), withId(R.id.basic_charge_fee_edit_text), isDisplayed()));
+        appCompatEditText.perform(replaceText("0.0254"), closeSoftKeyboard());
 
         ViewInteraction appCompatImageButton = onView(
                 allOf(withId(R.id.saveElectricFeeButton), isDisplayed()));
         appCompatImageButton.perform(click());
 
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.totalElectricFeeTextView)));
+        textView.check(matches(withText("$0.03")));
+
     }
 
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
 }
